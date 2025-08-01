391df2861ee13e8710e5f685ef188608ac2eb53d
Saroj Sapkota <118116220+sarojsap@users.noreply.github.com>
2025-08-01 15:47:15 +0000

    Updated Email Templates

--- tools/admin.py
+++ tools/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Tool
 
-# Register your models here.
+@admin.register(Tool)
+class ToolAdmin(admin.ModelAdmin):
+
+    # Fields to display in the main list view
+    list_display = ('name', 'owner', 'availability_status', 'average_rating', 'posted_time')
+    
+    # Filters that appear on the right-hand side
+    list_filter = ('availability_status', 'posted_time')
+    
+    # Fields to search by. We can even search by the owner's username.
+    search_fields = ('name', 'description', 'owner__username')
+    
+    # Enables the default "delete selected" action in the dropdown.
+    actions = ['delete_selected']
--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
-    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'tool_count']
+
+    def tool_count(self, obj):
+        count = obj.tool_set.count()
+        # The 'obj.pk' is the user's primary key (ID)
+        url = reverse('admin:tools_tool_changelist') + f'?owner__id__exact={obj.pk}'
+        return format_html('<a href="{}">{} Tools</a>', url, count)
+        
+    tool_count.short_description = 'Tools Owned' # Sets the column header text
 
 admin.site.register(CustomUser, CustomUserAdmin)
